bd53da31af20e6b8506477aa443d85de66d723d4
Stefano Harding <riddopic@gmail.com>
2016-10-02 09:37:04 -0700

    update elasticsearch

--- versions/2.4.1/config.mk
+++ versions/2.4.1/config.mk
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
 export ES_VERSION = 2.4.1
-export ES_SHA256 = 6a6acfc7bf7b4354dc6136daea54db1c844d632f
+export ES_SHA256 = 23a369ef42955c19aaaf9e34891eea3a055ed217d7fbe76da0998a7a54bbe167
 export ES_HOME = /usr/share/elasticsearch
 export ES_USER = elasticsearch
 export ES_GROUP = elasticsearch
